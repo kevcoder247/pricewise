89b59fc91f2df037818eb3d244b695169c49651d
kevin ventura <kevcoder@kevins-mbp.myfiosgateway.com>
2023-12-09 21:59:22 -0500

    wip: mongoose error fix

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -1,20 +1,21 @@
-import mongoose from "mongoose";
-//Track connection status
-let isConnected = false;
+import mongoose from 'mongoose';
+
+let isConnected = false;// Variable to track the connection status
 
 export const connectToDB = async () => {
   mongoose.set('strictQuery', true);
 
-  if(!process.env.MONGODB_URI)return console.log('MONGO_URI is not defined');
+  if(!process.env.MONGODB_URI) return console.log('MONGODB_URI is not defined');
 
-  if(isConnected) return console.log('=> using existing database connected');
+  if(isConnected) return console.log('=> using existing database connection');
 
-  try{
+  try {
     await mongoose.connect(process.env.MONGODB_URI);
 
     isConnected = true;
-    console.log('MongoDb Connected')
-  }catch (error){
+
+    console.log('MongoDB Connected');
+  } catch (error) {
     console.log(error)
   }
 }
